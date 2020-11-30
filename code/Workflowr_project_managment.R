ada95976e909da148a3e9c1f3f5b1be55ea3968a
jens-daniel-mueller <jensdaniel.mueller@usys.ethz.ch>
2020-11-30 11:35:58 +0100

    renamed preprocessed GLODAP file

--- code/Workflowr_project_managment.R
+++ code/Workflowr_project_managment.R
@@ -115,7 +115,7 @@ wflow_publish(here::here(
 message = "use setup child Rmd file")
 
 # commit regular changes (locally) and rebuild site
-wflow_publish(all = TRUE, message = "XXX")
+wflow_publish(all = TRUE, message = "renamed preprocessed GLODAP file")
 
 # commit changes including _site.yml (locally) and rebuild site
 wflow_publish(c("analysis/*Rmd"), message = "XXX", republish = TRUE)
