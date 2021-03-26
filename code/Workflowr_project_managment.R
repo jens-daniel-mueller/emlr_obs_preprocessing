96f4b3a1f92cdcb2e66101bf52ebe001b8a7d500
jens-daniel-mueller <jensdaniel.mueller@usys.ethz.ch>
2021-03-26 15:27:21 +0100

    darker grey for landmask

--- code/Workflowr_project_managment.R
+++ code/Workflowr_project_managment.R
@@ -5,7 +5,7 @@
 
 # commit regular changes (locally) and rebuild site
 # this takes only changed files into account
-wflow_publish(all = TRUE, message = "Canyon-B sourced from uptools")
+wflow_publish(all = TRUE, message = "darker grey for landmask")
 
 # commit changes including _site.yml (locally) and rebuild site in the specified order
 # you can also run this code with only some of the files. In this case remove the rebuild=TRUE command
