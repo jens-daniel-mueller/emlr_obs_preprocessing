1a3fb916ba0bc7e7885837c40725cf89c96492e0
jens-daniel-mueller <jensdaniel.mueller@usys.ethz.ch>
2021-08-06 13:06:39 +0200

    added 1 basin mask, included reccap2 atm co2

--- code/Workflowr_project_managment.R
+++ code/Workflowr_project_managment.R
@@ -5,7 +5,7 @@
 
 # commit regular changes (locally) and rebuild site
 # this takes only changed files into account
-wflow_publish(all = TRUE, message = "added ORCA depth_levels")
+wflow_publish(all = TRUE, message = "added 1 basin mask, included reccap2 atm co2")
 
 # commit changes including _site.yml (locally) and rebuild site in the specified order
 # you can also run this code with only some of the files. In this case remove the rebuild=TRUE command
