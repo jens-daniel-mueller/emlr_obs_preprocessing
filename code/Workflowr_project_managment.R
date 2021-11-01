2f367862d80e5bb44a4f9b8eecbbbee04c39278e
jens-daniel-mueller <jensdaniel.mueller@usys.ethz.ch>
2021-11-01 15:48:40 +0100

    preprocess adjustment table, create new basinmaps

--- code/Workflowr_project_managment.R
+++ code/Workflowr_project_managment.R
@@ -5,7 +5,7 @@
 
 # commit regular changes (locally) and rebuild site
 # this takes only changed files into account
-wflow_publish(all = TRUE, message = "preprocess tracers")
+wflow_publish(all = TRUE, message = "preprocess adjustment table, create new basinmaps")
 
 # commit changes including _site.yml (locally) and rebuild site in the specified order
 # you can also run this code with only some of the files. In this case remove the rebuild=TRUE command
