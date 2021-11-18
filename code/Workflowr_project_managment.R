1839007067a1ee758aa422fe9b9d162de2abeae2
jens-daniel-mueller <jensdaniel.mueller@usys.ethz.ch>
2021-11-18 15:57:13 +0100

    delta EW crossover values determined

--- code/Workflowr_project_managment.R
+++ code/Workflowr_project_managment.R
@@ -5,7 +5,7 @@
 
 # commit regular changes (locally) and rebuild site
 # this takes only changed files into account
-wflow_publish(all = TRUE, message = "delta crossover values determined")
+wflow_publish(all = TRUE, message = "delta EW crossover values determined")
 
 # commit changes including _site.yml (locally) and rebuild site in the specified order
 # you can also run this code with only some of the files. In this case remove the rebuild=TRUE command
