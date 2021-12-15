be2f94e6cdf948e4e720e156f1e48e6b55e2e75f
jens-daniel-mueller <jensdaniel.mueller@usys.ethz.ch>
2021-12-15 22:20:05 +0100

    analyse IO 1990 CRM data from Millero 1998 - TA only

--- code/Workflowr_project_managment.R
+++ code/Workflowr_project_managment.R
@@ -5,7 +5,7 @@
 
 # commit regular changes (locally) and rebuild site
 # this takes only changed files into account
-wflow_publish(all = TRUE, message = "analyse IO 1990 CRM data from Millero 1998")
+wflow_publish(all = TRUE, message = "analyse IO 1990 CRM data from Millero 1998 - TA only")
 
 # commit changes including _site.yml (locally) and rebuild site in the specified order
 # you can also run this code with only some of the files. In this case remove the rebuild=TRUE command
