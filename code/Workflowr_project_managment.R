953ac0a959330a93b60778eb52df0d1961b532c3
jens-daniel-mueller <jensdaniel.mueller@usys.ethz.ch>
2021-12-20 17:40:29 +0100

    revised missing cruise crossover analysis

--- code/Workflowr_project_managment.R
+++ code/Workflowr_project_managment.R
@@ -5,7 +5,7 @@
 
 # commit regular changes (locally) and rebuild site
 # this takes only changed files into account
-wflow_publish(all = TRUE, message = "restructured IO crossover analysis")
+wflow_publish(all = TRUE, message = "revised missing cruise crossover analysis")
 
 # commit changes including _site.yml (locally) and rebuild site in the specified order
 # you can also run this code with only some of the files. In this case remove the rebuild=TRUE command
