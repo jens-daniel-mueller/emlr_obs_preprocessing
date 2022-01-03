47811bd4cf2464d585ea7ccadb53d269ba646262
jens-daniel-mueller <jensdaniel.mueller@usys.ethz.ch>
2022-01-03 14:49:11 +0100

    plot crossover of gap filled data

--- code/Workflowr_project_managment.R
+++ code/Workflowr_project_managment.R
@@ -5,7 +5,7 @@
 
 # commit regular changes (locally) and rebuild site
 # this takes only changed files into account
-wflow_publish(all = TRUE, message = "plot regional time series and moving average")
+wflow_publish(all = TRUE, message = "plot crossover of gap filled data")
 
 # commit changes including _site.yml (locally) and rebuild site in the specified order
 # you can also run this code with only some of the files. In this case remove the rebuild=TRUE command
