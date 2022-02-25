4030fe6eb0186b58507763d3221ebe4f18560eec
jens-daniel-mueller <jensdaniel.mueller@usys.ethz.ch>
2022-02-25 08:49:31 +0100

    added cruise by cruise offset analysis

--- code/Workflowr_project_managment.R
+++ code/Workflowr_project_managment.R
@@ -5,7 +5,7 @@
 
 # commit regular changes (locally) and rebuild site
 # this takes only changed files into account
-wflow_publish(all = TRUE, message = "added S04 budget calculation")
+wflow_publish(all = TRUE, message = "added cruise by cruise offset analysis")
 
 # commit changes including _site.yml (locally) and rebuild site in the specified order
 # you can also run this code with only some of the files. In this case remove the rebuild=TRUE command
@@ -35,7 +35,7 @@ wflow_publish(here::here(
   c(
     "read_RECCAP2_flux_products.Rmd")
 ),
-message = "testrun new script")
+message = "read Seaflux data")
 
 
 # Push latest version to GitHub
