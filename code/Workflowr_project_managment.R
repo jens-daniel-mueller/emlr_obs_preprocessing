7e9cd4caad4bc214f0a4da61c0f03b47cc8c8ba0
jens-daniel-mueller <jensdaniel.mueller@usys.ethz.ch>
2022-03-14 15:19:49 +0100

    mean decadal offsets in cstar units

--- code/Workflowr_project_managment.R
+++ code/Workflowr_project_managment.R
@@ -5,7 +5,7 @@
 
 # commit regular changes (locally) and rebuild site
 # this takes only changed files into account
-wflow_publish(all = TRUE, message = "converted offsets to cstar units")
+wflow_publish(all = TRUE, message = "mean decadal offsets in cstar units")
 
 # commit changes including _site.yml (locally) and rebuild site in the specified order
 # you can also run this code with only some of the files. In this case remove the rebuild=TRUE command
