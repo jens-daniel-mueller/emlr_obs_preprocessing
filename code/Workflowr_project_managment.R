08d9b613b6ad5f094096aec381cf877be9b0ca0a
jens-daniel-mueller <jensdaniel.mueller@usys.ethz.ch>
2022-04-06 15:05:02 +0200

    fixed conversion error in cstar_tco2_talk

--- code/Workflowr_project_managment.R
+++ code/Workflowr_project_managment.R
@@ -5,7 +5,7 @@
 
 # commit regular changes (locally) and rebuild site
 # this takes only changed files into account
-wflow_publish(all = TRUE, message = "updated coverage maps for xover")
+wflow_publish(all = TRUE, message = "fixed conversion error in cstar_tco2_talk")
 
 # commit changes including _site.yml (locally) and rebuild site in the specified order
 # you can also run this code with only some of the files. In this case remove the rebuild=TRUE command
