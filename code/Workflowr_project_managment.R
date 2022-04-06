4508b0a99668e0e72d9d2caed7462992d2d9a028
jens-daniel-mueller <jensdaniel.mueller@usys.ethz.ch>
2022-04-06 15:53:01 +0200

    added decadal offset uncertainty

--- code/Workflowr_project_managment.R
+++ code/Workflowr_project_managment.R
@@ -5,7 +5,7 @@
 
 # commit regular changes (locally) and rebuild site
 # this takes only changed files into account
-wflow_publish(all = TRUE, message = "fixed conversion error in cstar_tco2_talk")
+wflow_publish(all = TRUE, message = "added decadal offset uncertainty")
 
 # commit changes including _site.yml (locally) and rebuild site in the specified order
 # you can also run this code with only some of the files. In this case remove the rebuild=TRUE command
