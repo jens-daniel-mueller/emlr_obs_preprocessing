7659624023414278015622225ba604e327eec71c
jens-daniel-mueller <jensdaniel.mueller@usys.ethz.ch>
2022-04-07 14:43:42 +0200

    removed cruise 18LU20080702

--- code/Workflowr_project_managment.R
+++ code/Workflowr_project_managment.R
@@ -5,7 +5,7 @@
 
 # commit regular changes (locally) and rebuild site
 # this takes only changed files into account
-wflow_publish(all = TRUE, message = "rerun all with lat max 65N and without arcic")
+wflow_publish(all = TRUE, message = "removed cruise 18LU20080702")
 
 # commit changes including _site.yml (locally) and rebuild site in the specified order
 # you can also run this code with only some of the files. In this case remove the rebuild=TRUE command
