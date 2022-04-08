86fd02234994aa7f8aca5f4d2bc865c84a36a4be
jens-daniel-mueller <jensdaniel.mueller@usys.ethz.ch>
2022-04-08 12:14:38 +0200

    compared CRM and xover results for IO 1990

--- code/Workflowr_project_managment.R
+++ code/Workflowr_project_managment.R
@@ -5,7 +5,7 @@
 
 # commit regular changes (locally) and rebuild site
 # this takes only changed files into account
-wflow_publish(all = TRUE, message = "removed cruise 18LU20080702")
+wflow_publish(all = TRUE, message = "compared CRM and xover results for IO 1990")
 
 # commit changes including _site.yml (locally) and rebuild site in the specified order
 # you can also run this code with only some of the files. In this case remove the rebuild=TRUE command
