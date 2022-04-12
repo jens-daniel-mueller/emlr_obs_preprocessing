88644e2351696c46962f196d28145c557127226f
jens-daniel-mueller <jensdaniel.mueller@usys.ethz.ch>
2022-04-12 11:31:26 +0200

    rerun including all cruises, except Indian and N Pacifc

--- code/Workflowr_project_managment.R
+++ code/Workflowr_project_managment.R
@@ -5,7 +5,7 @@
 
 # commit regular changes (locally) and rebuild site
 # this takes only changed files into account
-wflow_publish(all = TRUE, message = "testrun excluding all cruises extending beyond 40S")
+wflow_publish(all = TRUE, message = "rerun including all cruises, except Indian and N Pacifc")
 
 # commit changes including _site.yml (locally) and rebuild site in the specified order
 # you can also run this code with only some of the files. In this case remove the rebuild=TRUE command
