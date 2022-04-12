91374c3a4cfd1abb9f9381e41ba9f9f919d39460
jens-daniel-mueller <jensdaniel.mueller@usys.ethz.ch>
2022-04-12 10:34:12 +0200

    testrun excluding all cruises extending beyond 40S

--- code/Workflowr_project_managment.R
+++ code/Workflowr_project_managment.R
@@ -5,7 +5,7 @@
 
 # commit regular changes (locally) and rebuild site
 # this takes only changed files into account
-wflow_publish(all = TRUE, message = "revise xover assesment")
+wflow_publish(all = TRUE, message = "testrun excluding all cruises extending beyond 40S")
 
 # commit changes including _site.yml (locally) and rebuild site in the specified order
 # you can also run this code with only some of the files. In this case remove the rebuild=TRUE command
