d145737921e61d04cefb43cace823d260e044299
jens-daniel-mueller <jensdaniel.mueller@usys.ethz.ch>
2022-04-26 16:00:15 +0200

    write published, unmasked column inventories of G19 to files

--- code/Workflowr_project_managment.R
+++ code/Workflowr_project_managment.R
@@ -5,7 +5,7 @@
 
 # commit regular changes (locally) and rebuild site
 # this takes only changed files into account
-wflow_publish(all = TRUE, message = "revised plots")
+wflow_publish(all = TRUE, message = "write published, unmasked column inventories of G19 to files")
 
 # commit changes including _site.yml (locally) and rebuild site in the specified order
 # you can also run this code with only some of the files. In this case remove the rebuild=TRUE command
@@ -15,6 +15,7 @@ wflow_publish(here::here(
     "index.Rmd",
     "config_dependencies.Rmd",
     "config_parameterization.Rmd",
+    "read_GCB.Rmd",
     "read_regions.Rmd",
     "read_World_Ocean_Atlas_2018.Rmd",
     "read_GLODAPv2_2016_MappedClimatologies.Rmd",
@@ -27,7 +28,7 @@ wflow_publish(here::here(
     "read_RECCAP2_flux_products.Rmd"
   )
 ),
-message = "rerun all with lat max 65N and without arcic",
+message = "added GCB data read-in and rebuild",
 republish = TRUE)
 
 
