46f2c6b8862c4571bffa4cc731c7e28505f022b6
jens-daniel-mueller <jensdaniel.mueller@usys.ethz.ch>
2022-06-06 23:44:08 +0200

    included Nicos xover analysis for adjusted Knorr data

--- code/Workflowr_project_managment.R
+++ code/Workflowr_project_managment.R
@@ -5,7 +5,7 @@
 
 # commit regular changes (locally) and rebuild site
 # this takes only changed files into account
-wflow_publish(all = TRUE, message = "plots for GLODAP RG meeting")
+wflow_publish(all = TRUE, message = "included Nicos xover analysis for adjusted Knorr data")
 
 # commit changes including _site.yml (locally) and rebuild site in the specified order
 # you can also run this code with only some of the files. In this case remove the rebuild=TRUE command
@@ -30,6 +30,14 @@ wflow_publish(here::here(
 ),
 message = "added GCB data read-in and rebuild",
 republish = TRUE)
+
+wflow_publish(here::here(
+  "analysis",
+  c(
+    "read_GLODAPv2_2021.Rmd"
+  )
+),
+message = "revised NPO analysis for GLODAP")
 
 
 # Push latest version to GitHub
