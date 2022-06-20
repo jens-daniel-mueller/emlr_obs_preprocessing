2d09820e75a06203c2472b406c2e2199131355ef
jens-daniel-mueller <jensdaniel.mueller@usys.ethz.ch>
2022-06-20 12:24:58 +0200

    checked JMA adjustments

--- code/Workflowr_project_managment.R
+++ code/Workflowr_project_managment.R
@@ -6,7 +6,7 @@
 # commit regular changes (locally) and rebuild site
 # this takes only changed files into account
 wflow_publish(all = TRUE,
-              message = "corrected back conversion from cstar_talk to talk")
+              message = "checked JMA adjustments")
 
 # commit changes including _site.yml (locally) and rebuild site in the specified order
 # you can also run this code with only some of the files. In this case remove the rebuild=TRUE command
